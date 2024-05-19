38d482055f21dd14cc071be21e295c2fcec60188
Gülsüm Vural <gulsum-vural@yandex.com>
2024-05-19 16:57:03 +0300

    feat: create a form app with c#

---
+++ FirstFormApp/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace FirstFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            label5 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox4 = new GroupBox();
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(208, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(645, 237);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(18, 20);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(75, 29);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Gulet";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(141, 20);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(112, 29);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Motor Yat";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(18, 23);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(66, 29);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "14m";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(114, 23);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(66, 29);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "15m";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(210, 23);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(66, 29);
+            radioButton5.TabIndex = 5;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "16m";
+            radioButton5.UseVisualStyleBackColor = true;
+            radioButton5.CheckedChanged += radioButton5_CheckedChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(18, 26);
+            dateTimePicker1.MaxDate = new DateTime(2024, 10, 31, 0, 0, 0, 0);
+            dateTimePicker1.MinDate = new DateTime(2024, 4, 1, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(176, 32);
+            dateTimePicker1.TabIndex = 6;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CustomFormat = "yyyy-MM-dd";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.Location = new Point(19, 30);
+            dateTimePicker2.MaxDate = new DateTime(2024, 10, 31, 0, 0, 0, 0);
+            dateTimePicker2.MinDate = new DateTime(2024, 4, 1, 0, 0, 0, 0);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(182, 32);
+            dateTimePicker2.TabIndex = 9;
+            dateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 8F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.DarkRed;
+            label5.Location = new Point(319, 267);
+            label5.Name = "label5";
+            label5.Size = new Size(426, 13);
+            label5.TabIndex = 12;
+            label5.Text = "Tekne türlerine ve uzunluklarına göre fiyatlar yukarıdaki tabloda yer almaktadır. *";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(229, 298);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(294, 55);
+            groupBox1.TabIndex = 13;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tekne Türü Seçin";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton5);
+            groupBox2.Controls.Add(radioButton4);
+            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Location = new Point(545, 298);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(295, 58);
+            groupBox2.TabIndex = 14;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Tekne Uzunluğu Seçin";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(dateTimePicker1);
+            groupBox3.Location = new Point(229, 372);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(296, 64);
+            groupBox3.TabIndex = 15;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Başlangıç Tarihi Seçin";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(dateTimePicker2);
+            groupBox4.Location = new Point(544, 368);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(296, 68);
+            groupBox4.TabIndex = 16;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Bitiş Tarihi Seçin";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(478, 466);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 36);
+            button1.TabIndex = 21;
+            button1.Text = "Hesapla";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1053, 547);
+            Controls.Add(button1);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label5);
+            Controls.Add(pictureBox1);
+            Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label5;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ FirstFormApp/Form1.cs
@@ -0,0 +1,198 @@
+﻿namespace FirstFormApp
+{
+    public partial class Form1 : Form
+    {
+        string boatTourSelect = "";
+        int boatWidth;
+        DateTime startDate;
+        DateTime finishDate;
+        bool calculateActive = false;
+        int price = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            boatTourSelect = "Motor Yat";
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            boatTourSelect = "Gulet";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            boatWidth = 14;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            boatWidth = 15;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            boatWidth = 16;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            startDate = dateTimePicker1.Value;
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.MinDate = startDate;
+            finishDate = dateTimePicker2.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calculateActive = true;
+            System.Threading.Thread.Sleep(700);
+            Form2 f2 = new Form2(this);
+            f2.ShowDialog();
+        }
+
+        public string CalculateTotalPrice()
+        {
+            if (calculateActive == true)
+            {
+                DateCalculator calculator = new DateCalculator();
+                Dictionary<int, int> resultCalculateDaysPerMonth = calculator.CalculateDaysPerMonth(startDate, finishDate);
+
+                foreach (var item in resultCalculateDaysPerMonth)
+                {
+                    Console.WriteLine($"{item.Key}. ayda : {item.Value} gün");
+                }
+
+                int totalPrice = calculator.CalculatePerMonthPrice(boatTourSelect, boatWidth, resultCalculateDaysPerMonth);
+                price = totalPrice;
+
+            }
+
+            if(boatTourSelect == "Gulet")
+            {
+                return $"{price} €";
+
+            } else
+            {
+                return $"{price} ₺";
+            }
+
+        }
+    }
+
+    public class DateCalculator
+    {
+        public int CalculateTotalDays(DateTime startDate, DateTime endDate)
+        {
+            return (endDate - startDate).Days + 1;
+        }
+
+        public int CalculatePerMonthPrice(string boatTourInput, int boatWidth, Dictionary<int, int> daysPerMonth)
+        {
+
+            int[][] guletPrices = new int[3][]
+            {
+                  new int[] { 400, 450, 500, 550, 550, 500, 450 },
+                  new int[] { 500, 700, 750, 850, 900, 750, 700 },
+                  new int[] { 250, 400, 550, 650, 650, 550, 400 },
+            };
+
+            int[][] motorYachtPrices = new int[3][]
+            {
+                  new int[] { 600 , 750 ,900 ,1200 ,1200, 900, 750 },
+                  new int[] { 900 ,1000,1100 ,1200 ,1200, 1100, 1000 },
+                  new int[] { 1250, 1500, 1750, 2000, 2000, 1750, 1500 },
+            };
+
+            Dictionary<int, int[]> guletPricesBtMonths = new Dictionary<int, int[]>();
+            guletPricesBtMonths.Add(16, guletPrices[0]);
+            guletPricesBtMonths.Add(15, guletPrices[1]);
+            guletPricesBtMonths.Add(14, guletPrices[2]);
+
+            Dictionary<int, int[]> motorYatchPricesByMonths = new Dictionary<int, int[]>();
+            motorYatchPricesByMonths.Add(14, motorYachtPrices[0]);
+            motorYatchPricesByMonths.Add(15, motorYachtPrices[1]);
+            motorYatchPricesByMonths.Add(16, motorYachtPrices[2]);
+
+            int secilenPrice = 0;
+            int totalPrice = 0;
+
+            foreach (var item in daysPerMonth)
+            {
+
+                if (boatTourInput == "Gulet")
+                {
+                    secilenPrice = guletPricesBtMonths[boatWidth][item.Key - 4];
+                    totalPrice += secilenPrice * item.Value;
+                }
+                else
+                {
+                    secilenPrice = motorYatchPricesByMonths[boatWidth][item.Key - 4];
+                    totalPrice += secilenPrice * item.Value;
+                }
+            }
+
+            if (boatTourInput == "Gulet")
+            {
+                Console.WriteLine($"Toplam Fiyat: {totalPrice}£");
+            }
+            else
+            {
+                Console.WriteLine($"Toplam Fiyat: {totalPrice}₺");
+
+            }
+
+            return (totalPrice);
+        }
+
+        public Dictionary<int, int> CalculateDaysPerMonth(DateTime startDate, DateTime endDate)
+        {
+            Dictionary<int, int> daysPerMonth = new Dictionary<int, int>();
+            DateTime current = startDate;
+
+            while (current <= endDate)
+            {
+                int monthKeyInt = current.Month;
+                if (!daysPerMonth.ContainsKey(monthKeyInt))
+                {
+                    daysPerMonth[monthKeyInt] = 0;
+                }
+
+                DateTime nextMonth = new DateTime(current.Year, current.Month, 1).AddMonths(1);
+                DateTime periodEnd = (nextMonth <= endDate) ? nextMonth.AddDays(-1) : endDate;
+
+                int daysInCurrentMonth = (periodEnd - current).Days + 1;
+
+                daysPerMonth[monthKeyInt] += daysInCurrentMonth;
+
+                current = nextMonth;
+            }
+
+
+            return daysPerMonth;
+        }
+    }
+}
